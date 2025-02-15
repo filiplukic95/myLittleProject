2bbd895879dedf3ec21f881793688443c2211f2b
filiplukic95 <filiplukic95@gmail.com>
2025-02-15 17:49:58 +0100

    PozadinskiRad.py

--- Projekat Zavrsni rad/PozadinskiRad.py
+++ Projekat Zavrsni rad/PozadinskiRad.py
@@ -1,9 +1,4 @@
-import tkinter as tk
-import db
-import os
-from db import models as mdl
 from gui.prviProgram.MainWindow import MainWindow
-db.DBEngine.SETTINGS_FILE=os.getcwd() + "/settings.ini"
 window=MainWindow()
 window.layout()
 if __name__ == "__main__":
